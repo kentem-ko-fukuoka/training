3cbf86c58cd740defde33a70a73477a0fb569777
kentem-ko-fukuoka <ko-fukuoka@kentem.co.jp>
2024-03-07 14:49:53 +0900

    環境構築

---
+++ vitest.config.ts
@@ -0,0 +1,16 @@
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vitest/config'
+
+export default defineConfig({
+    plugins: [react()],
+    test: {
+        environment: 'jsdom',
+        globals: true,
+        include: ['./**/*.test.{ts,tsx}'],
+        setupFiles: './vitest.setup.ts',
+        coverage: {
+            reporter: ['html'],
+            reportsDirectory: './Vitest/coverage',
+        },
+    },
+})
