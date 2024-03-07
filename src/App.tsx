35a7eb2208fed3b0cc34f21eb1ec67bbe38fa825
kentem-ko-fukuoka <ko-fukuoka@kentem.co.jp>
2024-03-07 17:53:04 +0900

    Revert "MemoApp"
    This reverts commit 88db6194d836a33c403fb44e65fd4b70db407967.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import { useState } from "react";
 import Slideshow from "./components/Slideshow";
 import Bingo from "./components/Bingo";
-import MemoApp from "./components/MemoApp";
 
 const APP_NULL = '';
 const APP_BINGO = 'bingo';
-const APP_MEMO_APP = 'memo-app';
 const APP_SLIDESHOW = 'slideshow';
 
 const App = () => {
@@ -19,9 +17,6 @@ const App = () => {
       case APP_BINGO:
         return <Bingo />;
 
-      case APP_MEMO_APP:
-        return <MemoApp />;
-
       case APP_SLIDESHOW:
         return <Slideshow />;
 
@@ -34,7 +29,6 @@ const App = () => {
     <>
       <div>
         <button onClick={() => setCurrentApp(APP_BINGO)}>Bingo</button>
-        <button onClick={() => setCurrentApp(APP_MEMO_APP)}>MemoApp</button>
         <button onClick={() => setCurrentApp(APP_SLIDESHOW)}>Slideshow</button>
       </div>
       {app}
