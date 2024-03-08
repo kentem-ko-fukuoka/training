3d8362329ff6ce82b220fb5d7e1b972167fd527e
kentem-ko-fukuoka <ko-fukuoka@kentem.co.jp>
2024-03-08 15:49:45 +0900

    Calendarを追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import { useState } from "react";
 import Slideshow from "./components/Slideshow";
+import Calendar from "./components/Calendar";
 
 const APP_NULL = '';
 const APP_SLIDESHOW = 'slideshow';
+const APP_CALENDAR = 'calendar';
 
 const App = () => {
 
@@ -15,6 +17,9 @@ const App = () => {
       case APP_SLIDESHOW:
         return <Slideshow />;
 
+      case APP_CALENDAR:
+        return <Calendar />;
+
       default:
         return null;
     }
@@ -24,6 +29,7 @@ const App = () => {
     <>
       <div>
         <button onClick={() => setCurrentApp(APP_SLIDESHOW)}>Slideshow</button>
+        <button onClick={() => setCurrentApp(APP_CALENDAR)}>Calendar</button>
       </div>
       {app}
     </>
