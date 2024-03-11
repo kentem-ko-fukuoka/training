3b3e7e7564ab4411aefda822e12162aba6672907
kentem-ko-fukuoka <ko-fukuoka@kentem.co.jp>
2024-03-11 09:53:36 +0900

    Stopwatch

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import { useState } from "react";
 import Slideshow from "./components/Slideshow";
+import Stopwatch from "./components/Stopwatch";
+import { createGlobalStyle } from "styled-components";
 
 const APP_NULL = '';
 const APP_SLIDESHOW = 'slideshow';
+const APP_STOPWATCH = 'stopwatch';
 
 const App = () => {
 
@@ -15,6 +18,9 @@ const App = () => {
       case APP_SLIDESHOW:
         return <Slideshow />;
 
+      case APP_STOPWATCH:
+        return <Stopwatch />;
+
       default:
         return null;
     }
@@ -22,8 +28,10 @@ const App = () => {
 
   return (
     <>
+      <Global $isStopwach={currentApp === APP_STOPWATCH} />
       <div>
         <button onClick={() => setCurrentApp(APP_SLIDESHOW)}>Slideshow</button>
+        <button onClick={() => setCurrentApp(APP_STOPWATCH)}>Stopwatch</button>
       </div>
       {app}
     </>
@@ -31,3 +39,10 @@ const App = () => {
 }
 
 export default App;
+
+const Global = createGlobalStyle<{ $isStopwach: boolean }>`
+  body {
+    background-color: ${(props) => props.$isStopwach ? '#eee' : 'white'};
+    margin: 0;
+  }
+`;
